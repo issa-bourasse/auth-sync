5a0fbe4285c956b007653269c0d0d8262f6a88f3
issa-bourasse <141015850+issa-bourasse@users.noreply.github.com>
2026-04-09 13:20:06 +0100

    Update firebase.js

--- firebase front/src/firebase.js
+++ firebase front/src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDL3qvg1lb2hIWXAQaRvokNuJOnVhbTonw",
-  authDomain: "authgm-fe84f.firebaseapp.com",
-  projectId: "authgm-fe84f",
-  storageBucket: "authgm-fe84f.firebasestorage.app",
-  messagingSenderId: "597496218057",
-  appId: "1:597496218057:web:5275e4f2c564c0f9a1e818",
-  measurementId: "G-885PXVD6N8"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
